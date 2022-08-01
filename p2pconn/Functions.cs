0d8ee6e9d203b970479a295d2353631b1a305c76
ArtemZh <zhkhvartjom@rambler.ru>
2022-08-01 21:57:00 +0300

    [Lesson2] Added some shanges

--- p2pconn/Functions.cs
+++ p2pconn/Functions.cs
@@ -16,6 +16,14 @@
             var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
             return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
         }
+
+        /// <summary>
+        /// Added some changes
+        /// </summary>
+        /// <param name="byteArray"></param>
+        /// <returns></returns>
+
+
         #endregion
     
         #region "byte-s s-byte"
